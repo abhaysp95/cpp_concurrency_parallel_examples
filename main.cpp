e18f63fb8d4e6b54906b199307da6584b23a2c2e
abhaysp95 <abhaysp95@protonmail.com>
2024-02-20 09:16:28 +0530

    feat: learning some new std lib algos which support parallelism

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include <algorithm>
 #include <bits/chrono.h>
 #include <chrono>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
+#include <functional>
 #include <iostream>
 #include <execution>
+#include <iterator>
 #include <mutex>
 #include <numeric>
+#include <ostream>
 #include <pstl/glue_execution_defs.h>
 #include <random>
 #include <ratio>
@@ -122,15 +126,72 @@ uint64_t reduce_numbrs_parallel(const vector<T>& vec) {
 	return res;
 }
 
+/** test std::inclusive_scan and std::partial_sum */
+void partial_sum_and_inclusive_scan() {
+	vector<int> vec = {4, 5, 2, 3, 8, 7, 9};
+	vector<int> temp(vec);
+
+	// useful for prefix sum algos
+	// you can provide op and init as extra params too
+	partial_sum(all(temp), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	// inclusive scan is unordered partial sum (use it for cummulative and
+	// associative types only)
+	// you can provide op and init as extra params too
+	// inclusive_scan (applies op with unspecified grouping) will give
+	// non-deterministic results for non-associtive ops, but is fine
+	// non-commutative ops
+	temp.clear();
+	temp.insert(temp.begin(), all(vec));
+	inclusive_scan(execution::par_unseq, all(temp), ostream_iterator<int>(cout, " "));
+
+	// NOTE: there's also exclusive scan which will exclude ith element in op (sum) up to ith element
+}
+
+/** Fused algorithms:
+ * transform_reduce
+ * transform_inclusive_scan
+ * transform_exclusive_scan
+ *
+ * the rules for cummulative & associative ops also apply to these fused algos
+ * just like std::transform, there's binary version of std::transform_reduce also
+ */
+
+/** vector norm: sqrt. of the sum of the square of each element */
+void calculate_vector_norm() {
+	vector<int> vec = {4, 5, 2, 3, 8, 7, 9};
+
+	double norm = sqrt(transform_reduce(
+				execution::par_unseq, all(vec), double(0.0),
+				[](double a, double b) { return a + b; },
+				[](int x) { return x * x; }));
+	cout << norm << endl;
+}
+
+void calculate_dot_product() {
+	vector<int> vec1 = {4, 5, 2, 3, 8, 7, 9};
+	vector<int> vec2(vec1);
+	reverse(all(vec2));
+	copy(all(vec1), ostream_iterator<int>(cout, " "));
+	cout << '\n';
+	copy(all(vec2), ostream_iterator<int>(cout, " "));
+	cout << '\n';
+
+	long long dprod = transform_reduce(execution::par_unseq, all(vec1), vec2.begin(),
+			0L, plus<long>(), multiplies<int>());
+	cout << dprod << endl;
+}
+
 int main(void) {
 
 	random_device rd;
 	printf("Test size: %zu unsigned ints...\n", TESTSIZE);
 
-	vector<uint32_t> vec(TESTSIZE);
+	/* vector<uint32_t> vec(TESTSIZE);
 	for (auto& x: vec) {
 		x = static_cast<uint32_t>(rd());
-	}
+	} */
 
 	/* for (size_t i = 0; i < ITERCOUNT; i++) {
 		vector<uint32_t> temp(vec);
@@ -142,7 +203,7 @@ int main(void) {
 		time_sort_parallel(temp);
 	} */
 
-	for (size_t i = 0; i < ITERCOUNT; i++) {
+	/* for (size_t i = 0; i < ITERCOUNT; i++) {
 		add_numbers_serial(vec);
 	}
 
@@ -160,7 +221,13 @@ int main(void) {
 
 	for (size_t i = 0; i < ITERCOUNT; i++) {
 		reduce_numbrs_parallel(vec);
-	}
+	} */
+
+	// partial_sum_and_inclusive_scan();
+
+	calculate_vector_norm();
+
+	calculate_dot_product();
 
 	return 0;
 }
