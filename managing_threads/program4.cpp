342380cba2e714372b603751790c74aaa5b91548
abhaysp95 <abhaysp95@protonmail.com>
2024-08-11 00:33:03 +0530

    chore: Chapter2 Concurrency in Action
      - updated discription

--- managing_threads/program4.cpp
+++ managing_threads/program4.cpp
@@ -12,9 +12,9 @@ void oops(char const* param) {
   char buffer[1024];
   sprintf(buffer, "%s", param);
 
-  // If you still pass a it like this, it is going to get copied in internal
-  // storage of thread like this and then will be passed as rvalue to f()
-  // But now f() wants a non-const ref. and so you are going to get error
+  // If you still pass a it like we did in program3, it is going to get copied
+  // in internal storage of thread like this and then will be passed as rvalue
+  // to f() But now f() wants a non-const ref. and so you are going to get error
   // Well, the error says "std::thread arguments must be invocable after
   // conversion to rvalues" for such scenarios.
   // Shouldn't the error be like "can't pass rvalue to non-const ref." or
