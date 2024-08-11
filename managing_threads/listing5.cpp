816e75b5266c7cb1fe736e0e34d7def0e13c1923
abhaysp95 <abhaysp95@protonmail.com>
2024-08-11 17:18:20 +0530

    feat: Chapter2 Concurrency in Action
      - passing thread as parameter

--- managing_threads/listing5.cpp
+++ managing_threads/listing5.cpp
@@ -22,10 +22,27 @@ std::thread g() {
   return t;
 }
 
+// NOTE: similar way like return, std::thread can be passed by value (or rvalue) so that ownership is transferred
+
+void f1(std::thread t) {
+  std::cout << "reached f1\n";
+  t.join();
+}
+
+void g1(std::thread&& t) {
+  std::cout << "reached g1\n";
+  t.join();
+}
+
 int main(void) {
   f().join();
   std::thread gt = g();
   gt.join();
+
+  std::cout << "going for f1\n";
+  f1(std::thread(do_something, 1));
+  std::thread t(do_something, 2);
+  g1(std::move(t));
 
   return 0;
 }
